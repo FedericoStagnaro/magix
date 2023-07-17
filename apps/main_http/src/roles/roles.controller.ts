06d17f20a44dce7a919c9bc2a7571492ab337eaf
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2023-07-17 19:34:22 -0300

    update roles main app

--- apps/main_http/src/roles/roles.controller.ts
+++ apps/main_http/src/roles/roles.controller.ts
@@ -6,67 +6,33 @@ import {
   Patch,
   Param,
   Delete,
-  HttpException,
-  HttpStatus,
-  Res,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { Response } from 'express';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
-  async create(
-    @Body() createRoleDto: CreateRoleDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    try {
-      const result = await this.rolesService.create(createRoleDto);
-      res.status(HttpStatus.CREATED).send(result);
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
+  async create(@Body() createRoleDto: CreateRoleDto) {
+    return await this.rolesService.create(createRoleDto);
   }
 
   @Get()
-  async findAll(@Res({ passthrough: true }) res: Response) {
-    try {
-      const result = await this.rolesService.findAll();
-      res.status(HttpStatus.OK).send(result);
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
+  async findAll() {
+    return await this.rolesService.findAll();
   }
 
   @Get(':id')
-  async findOne(
-    @Param('id') id: string,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    try {
-      const result = await this.rolesService.findOne(+id);
-      res.status(HttpStatus.OK).send(result);
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
+  async findOne(@Param('id') id: string) {
+    return await this.rolesService.findOne(+id);
   }
 
   @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateRoleDto: UpdateRoleDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    try {
-      const result = await this.rolesService.update(+id, updateRoleDto);
-      res.status(HttpStatus.OK).send(result);
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
+  async update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+    return await this.rolesService.update(+id, updateRoleDto);
   }
 
   @Delete(':id')
--- apps/main_http/src/roles/roles.service.ts
+++ apps/main_http/src/roles/roles.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { ClientProxy } from '@nestjs/microservices';
@@ -9,42 +9,26 @@ export class RolesService {
   constructor(@Inject('USERS_SERVICE') private roleClient: ClientProxy) {}
 
   async create(createRoleDto: CreateRoleDto) {
-    try {
-      return await lastValueFrom(
-        this.roleClient.send('createRole', createRoleDto),
-      );
-    } catch (error) {
-      throw Error(error.message);
-    }
+    return await lastValueFrom(
+      this.roleClient.send('createRole', createRoleDto),
+    );
   }
 
   async findAll() {
-    try {
-      return await lastValueFrom(this.roleClient.send('findAllRoles', {}));
-    } catch (error) {
-      throw Error(error.message);
-    }
+    return await lastValueFrom(this.roleClient.send('findAllRoles', {}));
   }
 
   async findOne(id: number) {
-    try {
-      return await lastValueFrom(this.roleClient.send('findOneRole', id));
-    } catch (error) {
-      throw Error(error.message);
-    }
+    return await lastValueFrom(this.roleClient.send('findOneRole', id));
   }
 
   async update(id: number, updateRoleDto: UpdateRoleDto) {
-    try {
-      return await lastValueFrom(
-        this.roleClient.send('updateRole', {
-          ...updateRoleDto,
-          id,
-        }),
-      );
-    } catch (error) {
-      throw Error(error.message);
-    }
+    return await lastValueFrom(
+      this.roleClient.send('updateRole', {
+        ...updateRoleDto,
+        id,
+      }),
+    );
   }
 
   async remove(id: number) {
